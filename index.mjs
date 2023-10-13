05c11c6a2c66224a9a89d84826503c1077206c60
macdonst <simon.macdonald@gmail.com>
2023-10-12 21:56:36 -0400

    Light refactor
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- index.mjs
+++ index.mjs
@@ -2,50 +2,21 @@
 const CustomElementMixin = (superclass) => class extends superclass {
   constructor() {
     super()
-    // Removes style tags as they are already inserted into the head by SSR
-    // TODO: If only added dynamically in the browser we need to insert the style tag after running the style transform on it. As well as handle deduplication.
-    let tagName = customElements.getName ? customElements.getName(this.constructor) : this.registeredName
-    this.template.content.querySelectorAll('style')
-      .forEach((tag) => {
-        const rules = this.rulesForCssText(tag.textContent)
-        console.log(rules)
 
-        const sheet = new CSSStyleSheet();
-        for (let rule of rules) {
-          if (rule.conditionText) {
-            let selectorText = ''
-            for (let innerRule of rule.cssRules) {
-              let selectors = innerRule.selectorText.split(',')
-              selectorText = selectors.map(selector => {
-                if (selector.startsWith(':host')) {
-                  let a = selector.replace(':host', tagName)
-                  return innerRule.cssText.replace(innerRule.selectorText, a)
-                } else {
-                  let a = `${tagName} ${selector}`
-                  return innerRule.cssText.replace(innerRule.selectorText, a)
-                }
-              }).join(',')
-            }
-            console.log(`${rule.media ? '@media' : ''} ${rule.conditionText} { ${selectorText}}`)
-            sheet.insertRule(`${rule.media ? '@media' : ''} ${rule.conditionText} { ${selectorText}}`, sheet.cssRules.length)
-          } else {
-            let selectors = rule.selectorText.split(',')
-            let selectorText = selectors.map(selector => {
-              if (selector.startsWith(':host')) {
-                return selector.replace(':host', tagName)
-              } else {
-                return `${tagName} ${selector}`
-              }
-            }).join(',')
-            sheet.insertRule(rule.cssText.replace(rule.selectorText, selectorText), sheet.cssRules.length)
-          }
-        }
-        console.log(sheet)
-        document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet]
+    // Has this element been server side rendered
+    const enhanced = this.hasAttribute('enhanced')
 
-        this.template.content.removeChild(tag)
-      }
-      )
+    // Handle style tags
+    if (enhanced) {
+      // Removes style tags as they are already inserted into the head by SSR
+      this.template.content.querySelectorAll('style')
+        .forEach((tag) => { this.template.content.removeChild(tag) })
+    } else {
+      let tagName = customElements.getName ? customElements.getName(this.constructor) : this.registeredName
+      this.template.content.querySelectorAll('style')
+        .forEach((tag) => this.styleTransform({ tag, tagName}))
+    }
+
     // Removes script tags as they are already appended to the body by SSR
     // TODO: If only added dynamically in the browser we need to insert the script tag after running the script transform on it. As well as handle deduplication.
     this.template.content.querySelectorAll('script')
@@ -53,7 +24,6 @@ const CustomElementMixin = (superclass) => class extends superclass {
 
     // Expands the Custom Element with the template content
     const hasSlots = this.template.content.querySelectorAll('slot')?.length
-    const enhanced = this.hasAttribute('enhanced')
 
     // If the Custom Element was already expanded by SSR it will have the "enhanced" attribute so do not replaceChildren
     if (!enhanced && !hasSlots) {
@@ -62,6 +32,46 @@ const CustomElementMixin = (superclass) => class extends superclass {
     } else if (!enhanced && hasSlots) {
       this.innerHTML = expandSlots(this)
     }
+  }
+
+  styleTransform({tag, tagName}) {
+    const rules = this.rulesForCssText(tag.textContent)
+    console.log(rules)
+
+    const sheet = new CSSStyleSheet();
+    for (let rule of rules) {
+      if (rule.conditionText) {
+        let selectorText = ''
+        for (let innerRule of rule.cssRules) {
+          let selectors = innerRule.selectorText.split(',')
+          selectorText = selectors.map(selector => {
+            if (selector.startsWith(':host')) {
+              let a = selector.replace(':host', tagName)
+              return innerRule.cssText.replace(innerRule.selectorText, a)
+            } else {
+              let a = `${tagName} ${selector}`
+              return innerRule.cssText.replace(innerRule.selectorText, a)
+            }
+          }).join(',')
+        }
+        console.log(`${rule.media ? '@media' : ''} ${rule.conditionText} { ${selectorText}}`)
+        sheet.insertRule(`${rule.media ? '@media' : ''} ${rule.conditionText} { ${selectorText}}`, sheet.cssRules.length)
+      } else {
+        let selectors = rule.selectorText.split(',')
+        let selectorText = selectors.map(selector => {
+          if (selector.startsWith(':host')) {
+            return selector.replace(':host', tagName)
+          } else {
+            return `${tagName} ${selector}`
+          }
+        }).join(',')
+        sheet.insertRule(rule.cssText.replace(rule.selectorText, selectorText), sheet.cssRules.length)
+      }
+    }
+    console.log(sheet)
+    document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet]
+
+    this.template.content.removeChild(tag)
   }
 
   rulesForCssText(styleContent) {
@@ -108,7 +118,7 @@ const CustomElementMixin = (superclass) => class extends superclass {
     // Unnamed Slot
     unnamedSlot.slotNode?.replaceWith(unnamedSlot?.contentToSlot.join(''))
 
-    // Unused slots and default content 
+    // Unused slots and default content
     const unfilledUnnamedSlots = Array.from(fragment.shadowRoot.querySelectorAll('slot:not([name])'))
     const unfilledSlots = Array.from(fragment.shadowRoot.querySelectorAll('slot[name]'))
     unfilledSlots.forEach(slot => {
